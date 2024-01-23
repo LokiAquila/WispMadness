3ac3528e81e14cc69b8059de55c6f17fa8730ecf
Nolucci <112586749+Nolucci@users.noreply.github.com>
2024-01-23 02:06:21 +0100

    add menu

--- Scripts/Menu.cs
+++ Scripts/Menu.cs
@@ -3,29 +3,15 @@ using System;
 
 public partial class Menu : Control
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+	private void _on_play_pressed()
 	{
+		GetTree().ChangeSceneToFile("res://Scenes/level.tscn");
 	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	private void _on_options_pressed()
 	{
+		GetTree().ChangeSceneToFile("res://Scenes/options.tscn");
 	}
-	
-	private void _on_PlayPressed()
-	{
-		GetTree().Root.AddChild(level);
-	}
-
-	// Appelé lorsque le bouton "Options" est pressé
-	private void _on_OptionsPressed()
-	{
-		GetTree().ChangeScene("res://Scenes/options.tscn");
-	}
-
-	// Appelé lorsque le bouton "Quit" est pressé
-	private void _on_QuitPressed()
+	private void _on_quit_pressed()
 	{
 		GetTree().Quit();
 	}
