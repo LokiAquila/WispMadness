de3b0cd8d416c69dda7898079b28971417c01df1
LokiTheKiller <lokithekilleryt@gmail.com>
2024-01-24 06:42:58 +0100

    Audio adjustments

--- Scripts/SettingsMenu.cs
+++ Scripts/SettingsMenu.cs
@@ -3,6 +3,9 @@ using Godot;
 public partial class SettingsMenu : Control
 {
 
+	[Export(PropertyHint.Range,"0,100,1")]
+	private float DefaultVolume { get; set; } = 50f;
+
 	[Export]
 	private TextSlider masterVolumeSlider;
 
@@ -22,24 +25,32 @@ public partial class SettingsMenu : Control
 		musicBusIndex = AudioServer.GetBusIndex("Music");
 		sfxBusIndex = AudioServer.GetBusIndex("SFX");
 
+		masterVolumeSlider.Value = DefaultVolume;
+		musicVolumeSlider.Value = DefaultVolume;
+		sfxVolumeSlider.Value = DefaultVolume;
+
 		masterVolumeSlider.ValueChanged += UpdateMasterVolume;
 		musicVolumeSlider.ValueChanged += UpdateMusicVolume;
 		sfxVolumeSlider.ValueChanged += UpdateSFXVolume;
+
+		UpdateMasterVolume(DefaultVolume);
+		UpdateMusicVolume(DefaultVolume);
+		UpdateSFXVolume(DefaultVolume);
 	}
 
 	private void UpdateMasterVolume(double value)
 	{
-		AudioServer.SetBusVolumeDb(masterBusIndex, (float)Mathf.LinearToDb(value));
+		AudioServer.SetBusVolumeDb(masterBusIndex, (float)Mathf.LinearToDb(value / 100d));
 	}
 	
 	private void UpdateMusicVolume(double value)
 	{
-		AudioServer.SetBusVolumeDb(musicBusIndex, (float)Mathf.LinearToDb(value));
+		AudioServer.SetBusVolumeDb(musicBusIndex, (float)Mathf.LinearToDb(value / 100d));
 	}
 	
 	private void UpdateSFXVolume(double value)
 	{
-		AudioServer.SetBusVolumeDb(sfxBusIndex, (float)Mathf.LinearToDb(value));
+		AudioServer.SetBusVolumeDb(sfxBusIndex, (float)Mathf.LinearToDb(value / 100d));
 	}
 
 }
